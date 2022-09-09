a3c5a3de9316a6fd6dff3e583107eef87eb7c9d7
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-09-09 14:47:59 -0300

    Add files via upload

--- Altura.java
+++ Altura.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 public class Altura {
 
 	public static void main(String[] args) {
-		
-		double joao = 1.34, pedro = 1.45 , i = 1;
-		while (joao<=pedro) {
-			joao = (joao+0.025);
-			pedro = (pedro+0.020);
-		i++;
-		
+
+				double joao = 1.34, pedro = 1.45 , i = 1;
+				while (joao<=pedro) {
+					joao = (joao+0.025);
+					pedro = (pedro+0.020);
+				i++;
+				
+				}
+				System.out.println("daqui há" + i + "anos Joao será maior que Pedro");
 		}
-		System.out.println("ser�" +i+ "anos para o joao ser maior que pedro");
-}
-}
+		
+
+	}
+
