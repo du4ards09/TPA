4d53db2517e3002a75b1098344df4e59097af337
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-09-09 11:56:56 -0300

    Add files via upload

---
+++ Altura.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Altura {
+
+	public static void main(String[] args) {
+		
+		double joao = 1.34, pedro = 1.45 , i = 1;
+		while (joao<=pedro) {
+			joao = (joao+0.025);
+			pedro = (pedro+0.020);
+		i++;
+		
+		}
+		System.out.println("será" +i+ "anos para o joao ser maior que pedro");
+}
+}
