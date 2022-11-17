bbda3629afb78ab1dac7361b16b5ee115124e55e
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-11-17 18:57:55 -0300

    Create multiplo.java

---
+++ multiplo.java
@@ -0,0 +1,13 @@
+public class Multiplos {
+
+	public static void main(String[] args) {
+		int i=1,m;
+		do {
+			m = (i*10);
+			System.out.println(i);
+			System.out.println("Multiplo de 10: " +m);
+			i++;
+		}while(i<=100);
+	}
+
+}
