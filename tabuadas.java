39471933ff5190ef2b5eec56f68134c357937437
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-11-17 21:49:27 -0300

    Create tabuadas.java

---
+++ tabuadas.java
@@ -0,0 +1,14 @@
+package tabuadas;
+
+public class tabuada {
+
+	public static void main(String[] args) {
+		for (int n = 1; n <= 10; n++) {
+			System.out.println("           Tabuada do " + n);
+	
+		for (int i = 1; i <=10; i++) {
+			System.out.println(n + "x" + i + " = " + (n*1));
+		}
+		}
+     }
+}
