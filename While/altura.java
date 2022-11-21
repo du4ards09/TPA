1f787f3a7af09b5a37cdeef24f0434b90b699c2b
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-11-21 19:13:37 -0300

    Create altura.java

---
+++ While/altura.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Altura {
+
+	public static void main(String[] args) {
+
+				double joao = 1.34, pedro = 1.45 , i = 1;
+				while (joao<=pedro) {
+					joao = (joao+0.025);
+					pedro = (pedro+0.020);
+				i++;
+				
+				}
+				System.out.println("daqui há" + i + "anos Joao será maior que Pedro");
+		}
+		
+
+	}
