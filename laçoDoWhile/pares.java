5bdb994da1aab0277e00ff237d6a9cbcf334f9c0
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-11-21 19:08:25 -0300

    Create pares.java

---
+++ laçoDoWhile/pares.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class pares {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int i = 1;
+		boolean num1;
+		do{
+			if(i % 2 == 0);
+			num1 = (i<=500);
+			System.out.println(i);
+			++i;
+		}while(i<=500);
+
+			}
+			
+	}
